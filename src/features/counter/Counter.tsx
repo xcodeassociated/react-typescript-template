991d68ff061211c9c29988283fcb997394d08f76
xcodeassociated <janusz.majchrzak@protonmail.com>
2023-01-28 22:33:12 +0100

    add keycloak

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { useKeycloak } from '@react-keycloak/web'
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 import {
   decrement,
@@ -17,52 +17,69 @@ export const Counter: React.FC = () => {
   const [incrementAmount, setIncrementAmount] = useState('2');
 
   const incrementValue = Number(incrementAmount) || 0;
+  const { keycloak } = useKeycloak()
+  const { initialized } = useKeycloak()
+
+  if (!initialized) {
+    return <div>Loading...</div>
+  }
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
+      <div>
+        {!keycloak?.authenticated ?
+            <button type="button" onClick={() => keycloak.login()}>
+              Login
+            </button>
+            : <></>}
+
+        {!!keycloak?.authenticated && (
+            <button type="button" onClick={() => keycloak.logout()}>
+              Logout
+            </button>
+        )}
+        <div className={styles.row}>
+          <button
+              className={styles.button}
+              aria-label="Decrement value"
+              onClick={() => dispatch(decrement())}
+          >
+            -
+          </button>
+          <span className={styles.value}>{count}</span>
+          <button
+              className={styles.button}
+              aria-label="Increment value"
+              onClick={() => dispatch(increment())}
+          >
+            +
+          </button>
+        </div>
+        <div className={styles.row}>
+          <input
+              className={styles.textbox}
+              aria-label="Set increment amount"
+              value={incrementAmount}
+              onChange={(e) => setIncrementAmount(e.target.value)}
+          />
+          <button
+              className={styles.button}
+              onClick={() => dispatch(incrementByAmount(incrementValue))}
+          >
+            Add Amount
+          </button>
+          <button
+              className={styles.asyncButton}
+              onClick={() => dispatch(incrementAsync(incrementValue))}
+          >
+            Add Async
+          </button>
+          <button
+              className={styles.button}
+              onClick={() => dispatch(incrementIfOdd(incrementValue))}
+          >
+            Add If Odd
+          </button>
+        </div>
       </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,32 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import keycloak from "./features/counter/keycloak";
+import { ReactKeycloakProvider } from '@react-keycloak/web'
+import {AuthClientError, AuthClientEvent} from "@react-keycloak/core/lib/types";
+import {AuthClientTokens} from "@react-keycloak/core";
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+const onKeycloakEvent = (event: AuthClientEvent, error?: AuthClientError): void => {
+    if (error) {
+        console.error("Keycloak error: " + JSON.stringify(error))
+    }
+}
+
+const onKeycloakTokens = (tokens: AuthClientTokens): void => {
+    console.info("Keycloak token: " + JSON.stringify(tokens))
+}
+
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <ReactKeycloakProvider authClient={keycloak} onEvent={onKeycloakEvent} onTokens={onKeycloakTokens} initOptions={{onLoad: 'check-sso'}}>
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>
+</ReactKeycloakProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
