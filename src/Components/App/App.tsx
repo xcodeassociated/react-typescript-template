ceb9d771495bac46962ddf8161ddbb53d05c537e
xcodeassociated <janusz.majchrzak@protonmail.com>
2023-09-01 09:07:03 +0200

    change menu ant

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Counter} from "../Counter/Counter";
 import {useKeycloak} from "@react-keycloak/web";
 import {Navigate, Route, Routes} from "react-router-dom";
@@ -6,7 +6,11 @@ import {Home} from "../Home/Home"
 import {Error, Unauthorized} from "../Error/Error";
 import {MenuProvider} from "../Menu/MenuProvider";
 import {Users} from "../Users/Users";
-import {Layout} from "antd";
+import {Button, Layout, theme} from "antd";
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined
+} from '@ant-design/icons';
 
 const {Header, Content, Footer, Sider} = Layout
 
@@ -20,6 +24,10 @@ const ProtectedRoute = ({predicate, redirectPath = '/', children}) => {
 
 const App: React.FC = () => {
     const {initialized, keycloak} = useKeycloak()
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
 
     if (!initialized) {
         return <div>Loading...</div>
@@ -30,12 +38,27 @@ const App: React.FC = () => {
             <Layout>
                 <Header style={{display: 'flex', alignItems: 'center'}}>
                     <div className="demo-logo"/>
+                    <Button>Test</Button>
+                    <Button style={{ marginLeft: 'auto' }}>Test</Button>
                 </Header>
                 <Content style={{padding: '0 50px'}}>
                     <Layout style={{padding: '24px 0', background: "white"}}>
-                        <Sider style={{background: "white"}} width={200}>
+                        <Sider trigger={null} collapsible collapsed={collapsed}>
+                            <div className="demo-logo-vertical" />
                             <MenuProvider/>
                         </Sider>
+                        <Header style={{ padding: 0, background: colorBgContainer }}>
+                            <Button
+                                type="text"
+                                icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                                onClick={() => setCollapsed(!collapsed)}
+                                style={{
+                                    fontSize: '16px',
+                                    width: 64,
+                                    height: 64,
+                                }}
+                            />
+                        </Header>
                         <Content style={{padding: '0 24px', minHeight: 280}}>
                             <Routes>
                                 <Route path='*' element={<Error/>}/>
