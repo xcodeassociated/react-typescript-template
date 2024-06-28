ffe66d9a179f5c2fbc098e61c753eb3362c27963
xcodeassociated <janusz.majchrzak@protonmail.com>
2024-06-28 09:44:40 +0200

    add lang detection

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,6 +1,6 @@
 import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
-
+import LanguageDetector from 'i18next-browser-languagedetector'
 import translationEnglish from './lang/en_US.json'
 import translationPolish from './lang/pl_PL.json'
 
@@ -13,23 +13,34 @@ const resources = {
   },
 }
 
-i18next.use(initReactI18next).init({
-  resources,
-  debug: false,
-  lng: localStorage.getItem('lng') || 'en',
-  react: {
-    bindI18n: 'loaded languageChanged',
-    bindI18nStore: 'added',
-    useSuspense: true,
-  },
-  interpolation: {
-    escapeValue: false,
-  },
-})
+const DETECTION_OPTIONS = {
+  order: ['cookie', 'localStorage', 'navigator'],
+  caches: ['cookie', 'localStorage'],
+  lookupCookie: 'i18next',
+  lookupLocalStorage: 'i18nextLng',
+}
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    detection: DETECTION_OPTIONS,
+    resources,
+    debug: false,
+    fallbackLng: 'en',
+    react: {
+      bindI18n: 'loaded languageChanged',
+      bindI18nStore: 'added',
+      useSuspense: true,
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+  .then((e) => {})
 
 export const setLanguage = (lang: string) => {
-  i18next.changeLanguage(lang)
-  localStorage.setItem('lng', lang)
+  i18next.changeLanguage(lang).then((e) => {})
 }
 
 export default i18next
